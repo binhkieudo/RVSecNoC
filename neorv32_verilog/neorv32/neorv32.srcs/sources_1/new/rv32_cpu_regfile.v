4a2974c7c6db5a9aae0eda72539edd2f94430d28
binhkieudo <binhkieudo@gmail.com>
2023-10-06 18:25:19 +0900

    update

---
+++ neorv32_verilog/neorv32/neorv32.srcs/sources_1/new/rv32_cpu_regfile.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/06/2023 06:09:46 PM
+// Design Name: 
+// Module Name: rv32_cpu_regfile
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rv32_cpu_regfile(
+
+    );
+endmodule
